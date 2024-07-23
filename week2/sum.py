3daee9c41f32f4ce4f4d8fd89dfd1bb5bf2198b6
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-07-23 10:37:41 +0530

    Update sum.py

--- week2/sum.py
+++ week2/sum.py
@@ -1,5 +1,12 @@
 #sum of first ten natural numbers
 sum=0
-for i in range(1,11) :
-	sum=sum+i
-print("the sum of first ten natural num=",sum)
+i=1
+var=0 #var is  a variable which stores the value of the sum when using for loop
+while  i<11 :
+ 	sum=sum+i
+ 	i=i+1
+ 	
+print("sum of ten natural num=",sum)
+for i in range(11) :
+	var=var+i
+print("the sum of first ten natural num=",var)
