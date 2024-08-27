07b74f421bffc5e7fa818c4a6f984ab5da457e0a
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-08-27 10:36:28 +0530

    Create Basic_fun.py

---
+++ week6/Basic_fun.py
@@ -0,0 +1,21 @@
+def add():
+	a=int(input("enter value of a:"))
+	b=int(input("enter value of b:"))
+	print("Addition=",a+b)
+def sub():
+	a=int(input("enter value of a:"))
+	b=int(input("enter value of b:"))
+	print("subtraction=",a-b)
+def Mul():
+	a=int(input("enter value of a:"))
+	b=int(input("enter value of b:"))
+	print("Multiplication=",a*b)
+def Div():
+	a=int(input("enter value of a:"))
+	b=int(input("enter value of b:"))
+	print("Division=",a/b)
+add()
+sub()
+Mul()
+Div()	
+	
