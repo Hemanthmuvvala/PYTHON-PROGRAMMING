8145af738f8cad986f478201ed8d329520d57533
hemanth venkata naga pavan kumar <160225341+Hemanthmuvvala@users.noreply.github.com>
2024-09-24 11:42:58 +0530

    Update Append.py

--- week8/Append.py
+++ week8/Append.py
@@ -2,11 +2,8 @@ f=open('textfile.txt','w')
 f.write('hi hello This is write operator\n')
 l=['My name is\n','Hemanth\n','I am a student\n']
 f.writelines(l)
-f=open('textfile.txt','r')
-print(f.read())
-print(f.readline())
-print(f.readlines())
 with open('textfile.txt','a') as f:
-	f.write('He is an engineer\n')
+	f.write('Iam  an engineer\n')
+f=open('textfile.txt','r')
 print(f.read())
 f.close()
